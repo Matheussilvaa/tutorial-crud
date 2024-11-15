355af5aa35267081c9d2b083eb655b672c1fd041
Matheussilvaa <matheussilvabezerra10@gmail.com>
2024-11-15 15:00:59 -0300

    tutorial CRUD

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import UserList from "./UserList";
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD com React</h1>
+      <UserList />
+    </div>
+  );
+}
+export default App;
